2519f0d859b874fa67d4013e212c4b2c3cd1817a
Rich Sollenne <RichSollenne@Richs-MacBook-Pro.local>
2015-07-02 16:14:24 -0500

    completed dropdowns etc

--- _site/js/app.js
+++ _site/js/app.js
@@ -2,11 +2,10 @@
 
 $(document).ready(function() {
 
-
 	$("[name='remember-me-checkbox']").bootstrapSwitch();
 
 	$('.header-link, .header-links-dropdown').mouseenter(function() {
-		$('.header-links-dropdown').stop().animate({height: '155px'},275).addClass('open');
+		$('.header-links-dropdown').stop().animate({height: '170px'},275).addClass('open');
 		if ($(this).hasClass('header-link-about')) {
 			$('.about-dropdown').show();
 		}
@@ -54,10 +53,31 @@ $(document).ready(function() {
 	});
 
 
+	function _rxFadeIn(el) {
+		$('.overlay-content').each(function(){
+			$(this).stop().fadeOut(275);
+		});
+		$('.' + el).stop().fadeIn(275);
+	}
+
+	$('.overlay-target').mouseenter(function() {
+		if ($(this).hasClass('myaccount-target')) {
+			_rxFadeIn('myaccount-dropdown-wrapper');
+		}else if ($(this).hasClass('minicart-target')) {
+			_rxFadeIn('minicart-dropdown-wrapper');
+		}
+	});
+
+
+	$('.dropdown-action').mouseleave(function() {
+		$('.overlay-content').stop().fadeOut(275);
+	});
+
 });
 
 
-// THIS PART IS JUST FOR SHOWING LOGGED IN VS LOGGED OUT STATES
+// THIS PART IS JUST FOR SHOWING LOGGED IN VS LOGGED OUT STATES 
+// NOT FOR PRODUCTION
 if(window.location.href.indexOf("signed-in=yes") > -1) {
 	$('body').find('.signed-out').remove();	
 } else  {
